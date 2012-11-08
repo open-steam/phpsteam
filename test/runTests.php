69f23b4c5692275b6fb965cedb571531fc3a141c
Dominik Niehus <nicke@uni-paderborn.de>
2012-11-08 09:54:19 +0100

    refs #1951

---
+++ test/runTests.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+<?php
+
+$pathComposer = __DIR__ . "/composer/";
+
+echo "** Run composer **" . PHP_EOL;
+
+$returnStatus = false;
+exec('curl 2> /dev/null', $output, $returnStatus);
+if ($returnStatus == 127) {
+	die("Please install curl (e.g. apt-get install curl or brew install curl");
+}
+if (file_exists($pathComposer . "composer.phar")) {
+	passthru("cd $pathComposer; php composer.phar self-update");
+} else {
+	passthru("cd $pathComposer; curl -s https://getcomposer.org/installer | php");
+}
+
+passthru("cd $pathComposer; php composer.phar install");
