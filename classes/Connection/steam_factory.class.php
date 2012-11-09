76b6af9fea0e35ff64c2d46d284f802a408ef4e3
Dominik Niehus <nicke@uni-paderborn.de>
2012-11-09 12:26:36 +0100

    refs #476 * debugging

--- classes/Connection/steam_factory.class.php
+++ classes/Connection/steam_factory.class.php
@@ -546,14 +546,15 @@ class steam_factory {
 		}*/
 
 		if (!is_string($pSteamConnectorID)) throw new ParameterException("pSteamConnectorID", "string");
+
 		$textdoc = steam_factory::create_object($pSteamConnectorID, $pName, CLASS_DOCUMENT, $pEnvironment, array("mimetype" => $pMimeType, "attributes" => array(OBJ_DESC => $pDescription)));
 
-		if ($persistence_type !== false) {
+/*		if ($persistence_type !== false) {
 
 			$textdoc->set_content($pContent, 0, $persistence_type);
 		} else {
 			$textdoc->set_content($pContent);
-		}
+		}*/
 
 		return $textdoc;
 	}
