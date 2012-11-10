d2da6e895ae8a4051faa5591629097145a26e325
Dominik Niehus <nicke@uni-paderborn.de>
2012-11-10 22:26:38 +0100

    refs #476

--- classes/persistence/FileUidPersistence.class.php
+++ classes/persistence/FileUidPersistence.class.php
@@ -69,7 +69,7 @@ class FileUidPersistence extends FilePersistence {
 
         $steam_id = $document->get_id();
         file_put_contents($target_dir . $steam_id, $content);
-		$document->steam_command($document, "set_content", array($uuid), $buffer);
+		$document->steam_command($document, "set_content", array($uuid), 0);
 		if ($buffer) {
 			return $document->get_steam_connector()->add_to_buffer(strlen($content));
 		} else {
