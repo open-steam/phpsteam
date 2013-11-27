cfb31880067cd5bf04f5b5ec98e2b8ad1db4fc47
Dominik Niehus <dominik.niehus@coactum.de>
2013-11-28 00:10:46 +0100

    fixed inline downloader

--- classes/Persistence/Downloader/InlineDownloader.class.php
+++ classes/Persistence/Downloader/InlineDownloader.class.php
@@ -2,13 +2,13 @@
 
 namespace OpenSteam\Persistence\Downloader;
 
-class InlineDownloader extends Downloader {
-
+class InlineDownloader extends Downloader
+{
     protected static function prepare_header(\steam_document $document, $params = array())
     {
         header("Pragma: private");
         header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-        header("Content-Type: application/octet-stream");
+        header("Content-Type: {$document->get_mimetype()}");
         header("Content-Length:" . $document->get_content_size());
         header("Content-Disposition: inline; filename=\"" . $document->get_name() . "\"");
     }
