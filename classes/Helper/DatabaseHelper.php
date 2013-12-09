037290a45aed998cab17e62a02f407395355b9a1
Dominik Niehus <dominik.niehus@coactum.de>
2013-12-09 14:15:23 +0100

    cleanup

--- classes/Helper/DatabaseHelper.php
+++ classes/Helper/DatabaseHelper.php
@@ -226,44 +226,9 @@ class DatabaseHelper
         return $size;
     }
 
-    // TODO: Fill with values
-    public function get_document_attributes($id)
-    {
-        $mysql_data = array();
-        $data = array();
-        $rs = "";
-        $query = "select * from ob_data where (ob_attr='DOC_MIME_TYPE' or ob_attr='OBJ_NAME' or ob_attr='DOC_LAST_MODIFIED' or ob_attr='OBJ_CREATION_TIME') and ob_id = " . $id;
-        $result = mysql_query($query);
-        if ($result) {
-            while ($row = mysql_fetch_row($result)) {
-                // store key value pairs in temp array
-                //print("row[2]=" . $row[2] . " row[3]=" . $row[3]);
-                $mysql_data[$row[2]] = str_replace('"', '', $row[3]);
-            }
-        } else {
-            if (defined("LOG_DEBUGLOG")) {
-                $time1 = microtime(TRUE);
-                logging::write_log(LOG_DEBUGLOG, "download_handling::get_document_attributes\t" . $id . " \tresult is null fetching the needed attribute values");
-            }
-        }
-        $mime = "application/x-unknown-content-type";
-        if (isset($mysql_data[DOC_MIME_TYPE]) && $mysql_data[DOC_MIME_TYPE] != "") $mime = $mysql_data[DOC_MIME_TYPE];
-        $data["mimetype"] = $mime;
-
-        $lm = $mysql_data[OBJ_CREATION_TIME];
-        if (isset($mysql_data[DOC_LAST_MODIFIED]) && $mysql_data[DOC_MIME_TYPE] != 0) $lm = $mysql_data[DOC_LAST_MODIFIED];
-
-        $data["mimetype"] = $mime;
-        $data["lastmodified"] = $lm;
-        $data["name"] = $mysql_data[OBJ_NAME];
-        $data["contentsize"] = $this->get_content_size($this->get_content_id($id));
-
-        return $data;
-    }
-
         public function getUnreadMails($userName="")
     {
-        $link = mysql_connect(STEAM_DATABASE_HOST, STEAM_DATABASE_USER, STEAM_DATABASE_PASS, true);
+        //$link = mysql_connect(STEAM_DATABASE_HOST, STEAM_DATABASE_USER, STEAM_DATABASE_PASS, true);
         if (!$link) {
             API_DEBUG ? $GLOBALS["MONOLOG"]->addError('no connection: ' . mysql_error()) : "";
             die("Probleme mit der Datenbank. Wir arbeiten an einer L&ouml;sung.");
@@ -339,7 +304,7 @@ class DatabaseHelper
 
     public function countMails($userName = "")
     {
-        $link = mysql_connect(STEAM_DATABASE_HOST, STEAM_DATABASE_USER, STEAM_DATABASE_PASS, true);
+       // $link = mysql_connect(STEAM_DATABASE_HOST, STEAM_DATABASE_USER, STEAM_DATABASE_PASS, true);
         if (!$link) {
             error_log('no connection: ' . mysql_error());
             die("Probleme mit der Datenbank. Wir arbeiten an einer L&ouml;sung.");
@@ -378,59 +343,5 @@ class DatabaseHelper
         }
 
         return (count($objectNumbers) - 1);
-    }
-
-        public function check_permissions($user, $oid, $password)
-    {
-        $query = "select v from i_users where k='" . $user . "'";
-        $result = mysql_query($query);
-        $row = mysql_fetch_row($result);
-        $uid = $result[0];
-
-        // if we dont have uid we can get this from i_users
-        $query = "select v from i_security_cache where k='" . $oid . ":" . $uid . "'";
-        $result = mysql_query($query);
-        $row = mysql_fetch_row($result);
-        $permissions = $row[0]; // read permission is bit "1"
-        if (($permissions & 1) == 1) return 1; else {
-            $permission_denied = $permissions >> 16;
-            if (($permission_denied & 1) == 1) return 0; else {
-                $STEAM = steam_connector::connect(STEAM_SERVER, STEAM_PORT, $user, $password);
-                $document = steam_factory::get_object($STEAM->get_id(), (int) $oid, CLASS_OBJECT);
-                if ($document->check_access_read($STEAM->get_current_steam_user()) === 1) return 1;
-            }
-        }
-
-        return 0;
-    }
-
-    public function is_modified($last_modified_server, $last_modified_http)
-    {
-        $timestamp_http = date2UnixTimestamp($last_modified_http);
-        if ($last_modified_server > $timestamp_http) return 0;
-        return 1;
-    }
-
-    public function get_last_modified($oid)
-    {
-        $query = "select ob_id from ob_data where ob_attr='OBJ_PATH' and ob_data='\"" . $path . "\"'";
-        $result = mysql_query($query);
-        if (!$result) return "";
-        $row = mysql_fetch_row($result);
-
-        return $row[0];
-    }
-
-    public function get_mime_type($oid)
-    {
-        $query = "select ob_data from ob_data where ob_attr='DOC_MIME_TYPE' and ob_id = " . $oid;
-        $result = mysql_query($query);
-        if ($result) {
-            if ($row = mysql_fetch_row($result)) {
-                return substr($row[0], 1, -1);
-            }
-        }
-
-        return "application/x-unknown-content-type";
     }
 }
