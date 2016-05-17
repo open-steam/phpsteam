4415af278cb3709976bd938d0505f3bcda005178
Dominik Niehus <dominik.niehus@coactum.de>
2016-05-17 14:46:56 +0200

    feat(thumbnail): improved image type detection

--- classes/Helper/ThumbnailHelper.class.php
+++ classes/Helper/ThumbnailHelper.class.php
@@ -1,168 +1,172 @@
 <?php
 
-class ThumbnailHelper
-{
-    public static function createThumbnail(steam_document $document, &$content, $mime, $thumbnail_path, $width, $height)
-    {
-        if ($mime === 'application/octet-stream') {
-            $mime = MimetypeHelper::get_instance()->getMimeType($document->get_name());
-        }
-        if ($mime !== 'image/gif' && $mime !== 'image/jpeg' && $mime !== 'image/jpg' && $mime != 'image/png') {
-            self::renderPlaceholderImage("defektes Bild", 'ccc', '555', $width, $height);
-            die;
-        }
+class ThumbnailHelper {
+	public static function createThumbnail(steam_document $document, &$content, $mime, $thumbnail_path, $width, $height) {
+		if ($mime === 'application/octet-stream') {
+			$mime = MimetypeHelper::get_instance()->getMimeType($document->get_name());
+		}
+		if ($mime !== 'image/gif' && $mime !== 'image/jpeg' && $mime !== 'image/jpg' && $mime != 'image/png') {
+			self::renderPlaceholderImage("defektes Bild", 'ccc', '555', $width, $height);
+			die;
+		}
 
-        $type = MimetypeHelper::get_instance()->getExtension($mime);
+		$type = MimetypeHelper::get_instance()->getExtension($mime);
 
-        $path = THUMBNAIL_PATH . $document->get_id() . "_" . $width . "x" . $height . "." . $type;
+		$path = THUMBNAIL_PATH . $document->get_id() . "_" . $width . "x" . $height . "." . $type;
 
-        if ($width == false) {
-            $width = $height;
-        }
-        if ($height == false) {
-            $height = $width;
-        }
+		if ($width == false) {
+			$width = $height;
+		}
+		if ($height == false) {
+			$height = $width;
+		}
 
-        $f = $path . ".temp";
-        $fh = fopen($f, "w+");
-        fwrite($fh, $content);
-        fclose($fh);
+		$f = $path . ".temp";
+		$fh = fopen($f, "w+");
+		fwrite($fh, $content);
+		fclose($fh);
 
-        $imgInfo = @getimagesize($f);
-        $src_width = $imgInfo[0];
-        $src_height = $imgInfo[1];
+		$imgInfo = @getimagesize($f);
+		$src_width = $imgInfo[0];
+		$src_height = $imgInfo[1];
 
-        if (empty($src_width) || empty($src_height)) {
-            self::renderPlaceholderImage("defektes Bild", 'ccc', '555', $width, $height);
-            die;
-        }
+		if (empty($src_width) || empty($src_height)) {
+			self::renderPlaceholderImage("defektes Bild", 'ccc', '555', $width, $height);
+			die;
+		}
 
-        //use correct aspect-ratio
-        $nWidth = $width;
-        $nHeight = $src_height * ($width / $src_width);
+		//use correct aspect-ratio
+		$nWidth = $width;
+		$nHeight = $src_height * ($width / $src_width);
 
-        //scale to match bounds
-        if ($nHeight > $height) {
-            $scale = $height / $nHeight;
-            $nHeight = $height;
+		//scale to match bounds
+		if ($nHeight > $height) {
+			$scale = $height / $nHeight;
+			$nHeight = $height;
 
-            $nWidth = $nWidth * $scale;
-        }
-        if ($nWidth > $width) {
-            $scale = $width / $nWidth;
-            $nWidth = $width;
+			$nWidth = $nWidth * $scale;
+		}
+		if ($nWidth > $width) {
+			$scale = $width / $nWidth;
+			$nWidth = $width;
 
-            $nHeight = $nHeight * $scale;
-        }
+			$nHeight = $nHeight * $scale;
+		}
 
-        $nWidth = round($nWidth);
-        $nHeight = round($nHeight);
+		$nWidth = round($nWidth);
+		$nHeight = round($nHeight);
 
-        $imgFunc = '';
+		$imgFunc = '';
 
-        if ($mime == 'image/gif' OR $type == 'gif') {
-            $img = ImageCreateFromGIF($f);
-            if (!$img) {
-                self::renderPlaceholderImage("defektes Bild", 'ccc', '555', $width, $height);
-                unlink($f);
-                die;
-            }
-            $imgFunc = 'ImageGIF';
-            $transparent_index = ImageColorTransparent($img);
-            if ($transparent_index != (-1))
-                $transparent_color = ImageColorsForIndex($img, $transparent_index);
-        } elseif ($mime == 'image/jpeg' OR $mime == 'image/jpg' OR $mime == 'image/pjpeg' OR $type == 'jpg') {
-            $img = ImageCreateFromJPEG($f);
-            if (!$img) {
-                self::renderPlaceholderImage("defektes Bild", 'ccc', '555', $width, $height);
-                unlink($f);
-                die;
-            }
-            $imgFunc = 'ImageJPEG';
-        } elseif ($mime == 'image/png' OR $type == 'png') {
-            $img = ImageCreateFromPNG($f);
-            if (!$img) {
-                self::renderPlaceholderImage("defektes Bild", 'ccc', '555', $width, $height);
-                unlink($f);
-                die;
-            }
-            ImageAlphaBlending($img, false);
-            ImageSaveAlpha($img, true);
+		$exifImageType = exif_imagetype($f);
 
-            $imgFunc = 'ImagePNG';
-        }
+		if ($exifImageType === IMAGETYPE_GIF) {
+			$img = ImageCreateFromGIF($f);
+			if (!$img) {
+				self::renderPlaceholderImage("defektes Bild", 'ccc', '555', $width, $height);
+				unlink($f);
+				die;
+			}
+			$imgFunc = 'ImageGIF';
+			$transparent_index = ImageColorTransparent($img);
+			if ($transparent_index != (-1)) {
+				$transparent_color = ImageColorsForIndex($img, $transparent_index);
+			}
 
-        $img_resized = ImageCreateTrueColor($nWidth, $nHeight);
-        if ($type == 'png') {
+		} elseif ($exifImageType === IMAGETYPE_JPEG) {
+			$img = ImageCreateFromJPEG($f);
+			if (!$img) {
+				self::renderPlaceholderImage("defektes Bild", 'ccc', '555', $width, $height);
+				unlink($f);
+				die;
+			}
+			$imgFunc = 'ImageJPEG';
+		} elseif ($exifImageType === IMAGETYPE_PNG) {
+			$img = ImageCreateFromPNG($f);
+			if (!$img) {
+				self::renderPlaceholderImage("defektes Bild", 'ccc', '555', $width, $height);
+				unlink($f);
+				die;
+			}
+			ImageAlphaBlending($img, false);
+			ImageSaveAlpha($img, true);
 
-            ImageAlphaBlending($img_resized, false);
-            ImageSaveAlpha($img_resized, true);
-            $transparent = imagecolorallocatealpha($img_resized, 255, 255, 255, 127);
-            imagefilledrectangle($img_resized, 0, 0, $nWidth, $nHeight, $transparent);
-        }
-        if (!empty($transparent_color)) {
-            $transparent_new = ImageColorAllocate($img_resized, $transparent_color['red'], $transparent_color['green'], $transparent_color['blue']);
-            $transparent_new_index = ImageColorTransparent($img_resized, $transparent_new);
-            ImageFill($img_resized, 0, 0, $transparent_new_index);
-        }
-        if (imagecopyresampled($img_resized, $img, 0, 0, 0, 0, $nWidth, $nHeight, $src_width, $src_height)) {
-            ImageDestroy($img);
-            $img = $img_resized;
-        }
+			$imgFunc = 'ImagePNG';
+		} else {
+			self::renderPlaceholderImage("defektes Bild", 'ccc', '555', $width, $height);
+			unlink($f);
+			die;
+		}
 
-        $imgFunc($img, $path);
-        ImageDestroy($img);
-        unlink($f);
+		$img_resized = ImageCreateTrueColor($nWidth, $nHeight);
+		if ($exifImageType === IMAGETYPE_PNG) {
 
-        return $path;
-    }
+			ImageAlphaBlending($img_resized, false);
+			ImageSaveAlpha($img_resized, true);
+			$transparent = imagecolorallocatealpha($img_resized, 255, 255, 255, 127);
+			imagefilledrectangle($img_resized, 0, 0, $nWidth, $nHeight, $transparent);
+		}
+		if (!empty($transparent_color)) {
+			$transparent_new = ImageColorAllocate($img_resized, $transparent_color['red'], $transparent_color['green'], $transparent_color['blue']);
+			$transparent_new_index = ImageColorTransparent($img_resized, $transparent_new);
+			ImageFill($img_resized, 0, 0, $transparent_new_index);
+		}
+		if (imagecopyresampled($img_resized, $img, 0, 0, 0, 0, $nWidth, $nHeight, $src_width, $src_height)) {
+			ImageDestroy($img);
+			$img = $img_resized;
+		}
 
-    public static function renderPlaceholderImage($message = 'Platzhalter', $bgColor = 'ccc', $fgColor = '555', $width = 100, $height = 100)
-    {
-        header ("Content-type: image/png");
+		$imgFunc($img, $path);
+		ImageDestroy($img);
+		unlink($f);
 
-        $image = imagecreate($width, $height);
+		return $path;
+	}
 
-        $bg = self::hex2rgb($bgColor);
-        $setbg = imagecolorallocate($image, $bg['r'], $bg['g'], $bg['b']);
+	public static function renderPlaceholderImage($message = 'Platzhalter', $bgColor = 'ccc', $fgColor = '555', $width = 100, $height = 100) {
+		header("Content-type: image/png");
 
-        $fg = self::hex2rgb($fgColor);
-        $setfg = imagecolorallocate($image, $fg['r'], $fg['g'], $fg['b']);
+		$image = imagecreate($width, $height);
 
-        $text = $message;
+		$bg = self::hex2rgb($bgColor);
+		$setbg = imagecolorallocate($image, $bg['r'], $bg['g'], $bg['b']);
 
-        $fontsize = 4;
-        $fontwidth = imagefontwidth($fontsize);
-        $fontheight = imagefontheight($fontsize);
-        $length = strlen($text);
-        $textwidth = $length * $fontwidth;
-        if ($textwidth > $width) {
-            $text = "X";
-            $length = strlen($text);
-            $textwidth = $length * $fontwidth;
-        }
-        $xpos = (imagesx($image) - $textwidth) / 2;
-        $ypos = (imagesy($image) - $fontheight) / 2;
+		$fg = self::hex2rgb($fgColor);
+		$setfg = imagecolorallocate($image, $fg['r'], $fg['g'], $fg['b']);
 
-        imagestring($image, $fontsize, $xpos, $ypos, $text, $setfg);
+		$text = $message;
 
-        imagepng($image);
-    }
+		$fontsize = 4;
+		$fontwidth = imagefontwidth($fontsize);
+		$fontheight = imagefontheight($fontsize);
+		$length = strlen($text);
+		$textwidth = $length * $fontwidth;
+		if ($textwidth > $width) {
+			$text = "X";
+			$length = strlen($text);
+			$textwidth = $length * $fontwidth;
+		}
+		$xpos = (imagesx($image) - $textwidth) / 2;
+		$ypos = (imagesy($image) - $fontheight) / 2;
 
-    public static function hex2rgb($colour)
-    {
-        $colour = preg_replace("/[^abcdef0-9]/i", "", $colour);
-        if (strlen($colour) == 6) {
-            list($r, $g, $b) = str_split($colour, 2);
+		imagestring($image, $fontsize, $xpos, $ypos, $text, $setfg);
 
-            return Array("r" => hexdec($r), "g" => hexdec($g), "b" => hexdec($b));
-        } elseif (strlen($colour) == 3) {
-            list($r, $g, $b) = array($colour[0] . $colour[0], $colour[1] . $colour[1], $colour[2] . $colour[2]);
+		imagepng($image);
+	}
 
-            return Array("r" => hexdec($r), "g" => hexdec($g), "b" => hexdec($b));
-        }
+	public static function hex2rgb($colour) {
+		$colour = preg_replace("/[^abcdef0-9]/i", "", $colour);
+		if (strlen($colour) == 6) {
+			list($r, $g, $b) = str_split($colour, 2);
 
-        return false;
-    }
+			return Array("r" => hexdec($r), "g" => hexdec($g), "b" => hexdec($b));
+		} elseif (strlen($colour) == 3) {
+			list($r, $g, $b) = array($colour[0] . $colour[0], $colour[1] . $colour[1], $colour[2] . $colour[2]);
+
+			return Array("r" => hexdec($r), "g" => hexdec($g), "b" => hexdec($b));
+		}
+
+		return false;
+	}
 
 }
